cdcee73579733fa1c6c2d3c27fc7d53e08ea98ab
Claudia Gutierrez Escribano <claudia@OPL-EUITI.upm.es>
2017-03-29 13:54:33 +0200

    calc code

---
+++ sim20032009/calc/fooProd.R
@@ -0,0 +1,31 @@
+## data is a vector concatenating the latitude value, the vector of 
+## irradiation values, and the vector of temperature values. See
+## example below
+fooProd <- function(data, modeTrk = 'fixed', timePeriod = 'year'){
+    ## Number of days
+    n <- (length(data) - 1)/2
+    lat <- data[1]
+    g0 <- data[2:(n + 1)]
+    Ta <- data[(n + 2):(2*n + 1)]
+    BD <- zoo(data.frame(G0 = g0, Ta = Ta),
+              order.by = tt)
+    Prod <- prodGCPV(lat = lat,
+                     modeRad = 'bd',
+                     dataRad= list(lat = lat, file = BD),
+                     keep.night=FALSE, modeTrk = modeTrk)
+    switch(timePeriod,
+           year = as.data.frameY(Prod)['Yf'],
+           month = as.data.frameM(Prod)['Yf']
+           )
+}
+
+## ## Example with a cell
+
+## iCell <- 10000
+## xG <- SISS[iCell]
+## xT <- Tas[iCell]
+## lat <- y[iCell]
+## xx <- c(lat, xG, xT)
+
+## xProd <- fooProd(xx, timePeriod = 'year')
+## xProd <- fooProd(xx, timePeriod = 'month')
