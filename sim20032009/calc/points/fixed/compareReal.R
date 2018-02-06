3890233a6017fe352b3159446d9a1682abbfb947
Claudia Gutiérrez Escribano <claudia.g.escribano@gmail.com>
2018-02-06 10:04:16 +0100

    elimina codigo sin usar

--- sim20032009/calc/points/fixed/compareReal.R
+++ sim20032009/calc/points/fixed/compareReal.R
@@ -2,7 +2,7 @@ library(raster)
 library(rasterVis)
 library(zoo)
 
-## This script will compare real PV data with the simulatios of model with/without aerosols
+## This script will compare real PV data with the simulations of model with/without aerosols
 
 ## ## load the real data ## ##
 ##############################
@@ -27,65 +27,9 @@ p <-aggregate(photocampa$Yf, by=as.yearmon, FUN=function(x) length(x))
 
 ## La energía Yf es la que tengo que comparar con los datos simulados. Algunos de los valores menores de lo esperado debido a paradas por mantenimiento, mejora etc. Por ello, podemos simular con la Gefectiva para compara con la simulada con datos del modelo.
 
-
-## extract at the point in the stack ##
-#######################################
-
-## Las simulaciones generales tienen un sistema que no se corresponde con la planta real, es genérico. Si se quiere, se puede extraer la información de un raster en una latitud concreta con el código a continuación, pero para una comparación hay que simular la generación en un putno con las características del sistema concreto. Los resultados de esto es la salida de pvpoint.R
-
-## 1. Hay que asignar al raster la proyección que le corresponde. Para CAER es LCC, para el satélite es long/lat.
- 
-mycrs <- CRS("+proj=lcc +lat_1=43 +lat_2=43 +lat_0=43 +lon_0=15 +k=0.684241 +units=m +datum=WGS84 +no_defs")
-
-## mascara data to project pvoutput data
-
-mascara <- raster("../../../figs/masque_terre_mer.nc", varname='zon_new')
-maslat <- raster("../../../figs/masque_terre_mer.nc", varname='lat')
-maslon <- raster("../../../figs/masque_terre_mer.nc", varname='lon')
-
-pmaslat <- rasterToPoints(maslat)
-pmaslon <- rasterToPoints(maslon)
-maslonlat <- cbind(pmaslon[,3], pmaslat[,3])
- 
-# Specify the lonlat as spatial points with projection as long/lat
-maslonlat <- SpatialPoints(maslonlat, proj4string = CRS("+proj=longlat +datum=WGS84"))
-pmaslonlat <- spTransform(maslonlat, CRSobj = mycrs)
-
-projection(mascara) <- mycrs
-extent(mascara) <- extent(pmaslonlat)
-
-#######
-## CAER
-#######
- 
-fixed <- stack("../../proj12abr/fixed_caer_monthlyProd_temp_20032009.grd")
-idx <- seq(as.Date("2003-01-01"), as.Date("2009-12-31"), 'month')
-fixed <- setZ(fixed, idx)
- 
-## defino el raster del modelo bien:
-
-projection(fixed) <- projection(mascara)
-extent(fixed) <- extent(mascara)
-
-## Hago las medias anuales de la simulación C-AER
-
-fixedMeses <- zApply(fixed, by=as.yearmon, fun='mean')
-
-## EXTRAER AQUI
-
-## Para extraer los puntos del modelo necesito un Spatialdata con la lat y la lon primero.
-## Ahora proyecto los puntos que quiero extraer a LCC
-
-bsrnlonlat <- SpatialPoints(cbind(lon,lat), proj4string = CRS("+proj=longlat +datum=WGS84"))
-bsrnlonlat <- spTransform(bsrnlonlat, mycrs)
-
-fixedMeses_caer <- extract(fixedMeses, bsrnlonlat, method="simple")
-
-photocampa_fixedMeses_caer <- as.zoo(t(fixedMeses_caer), as.yearmon(idx))
-
 ##############################################################################################
 
-## Se puede extraer el valor del AOD en ese punto concreto
+## Se puede extraer el valor del AOD en ese punto concreto para añadirlo a la comparación. Habría que proyectar el raster y extraer el pto.
 
 ######### AOD ##########
 #######################
@@ -103,33 +47,12 @@ extent(aod) <- extent(mascara)
 aod <- extract(aod, bsrnlonlat, method="simple")
 photocampa_aod <- as.zoo(t(aod), as.yearmon(idx))
 
-##_______________________________________________________________________________________
-## ESTE CÓDIGO NO SE USA:
-#c <- merge(photocampaMon, photocampa_fixedMeses_sat, photocampa_fixedMeses_caer, photocampa_fixedMeses_cno, photocampa_aod, all=FALSE) 
-#names(c) <- c("REAL", "SAT","CAER", "CNO", "AOD")
-
-#c2 <- c[-18,]
-  
-#pdf("seriesPhotocampaAOD.pdf")
-#xyplot(c,screens=c(1,1,1,1,2),scales = list(x = list(at = index(c), rot=45)), type='b', superpose=TRUE)
-#dev.off()
-
-##  con los dias que hay por mes:
- 
-#dias <- p[,1]
-
-#c <- merge(photocampaMon, photocampa_fixedMeses_sat, photocampa_fixedMeses_caer, photocampa_fixedMeses_cno, photocampa_aod, dias, all=FALSE) 
-#names(c) <- c("REAL", "SAT","CAER", "CNO", "AOD", "Days")
-
-#pdf("seriesPhotocampaAOD3.pdf")
-#xyplot(c,screens=c(1,1,1,1,2,3),scales = list(x = list(at = index(c), rot=45)), type='b', superpose=TRUE)
-#dev.off()
-
 ##______________________________________________________________________________________-
 
 ## 2. COMPARACIÓN CON LA SIMULACIÓN CON DATOS DEL pv system CORRECTOS
 
-## comparación con una simulacion de CAER con los datos del sistema corregidos################
+## comparación con una simulacion de CAER con los datos del sistema corregidos  ##########
+## xProd etc. son el resultado de pvpoint.R
 
 tt <- seq(as.Date('2003-01-01'), as.Date('2009-12-31'), 'month')
  
@@ -141,10 +64,10 @@ photocampa_fixedMeses_caer <- xProd
 photocampa_fixedMeses_no <- xProdno
 photocampa_fixed_sat <- xProdsat
  
-c <- merge(photocampaMon, photocampa_fixedMeses_caer, photocampa_fixedMeses_no, photocampa_fixed_sat, all=FALSE)#, photocampa_fixed_sat)dias, photocampa_aod, all=FALSE)
-#c <- c[which(index(c) >= "ene 2003" & index(c) <= "dic 2003")]
+c <- merge(photocampaMon, photocampa_fixedMeses_caer, photocampa_fixedMeses_no, photocampa_fixed_sat, all=FALSE)
 names(c) <- c("REAL", "CAER", "CNO", "SAT")
-#names(c) <- c("REAL", "CAER_SIS", "CAER_GEN", "DAYS", "AOD") 
+
+## Theme for plots
 
 myTheme <- custom.theme.2()
 myTheme$strip.background$col <- 'transparent'
@@ -182,7 +105,7 @@ maeNO <- mean(c$CNO - c$REAL)
 
 ## Elimino los datos de producción reales por debajo de 1
 
-d2 <- d[-15,]
+d2 <- d[-15,] 
 c2 <- c[-15,]
 
 pdf("modelsreal2.pdf")
@@ -247,7 +170,7 @@ dev.off()
 
 
 err3 <- cbind(c2$CAER-c2$REAL, c2$CNO-c2$REAL, c2$SAT-c2$REAL)
-names(err3) <- c("CAER", "CNO", "SAT")
+names(err3) <- c("AER", "NO-AER", "SAT")
 
 ## summary(err3)
 ##      Index           CAER              CNO              SAT         
@@ -279,7 +202,45 @@ xyplot(value~variable, groups=variable,data=dferr2, type='p', ylab='Error kWh/kW
 dev.off()
 
 
+pdf("pruebasviolinFixed.pdf")
+bwplot(value~variable, groups=variable,data=dferrPhoto, ylab='Error kWh/kWp', xlab='model', ylim=c(-0.7,2.3),
+       panel = function(..., cex,box.ratio) {
+           panel.violin(..., col='transparent',
+ varwidth = FALSE, box.ratio = box.ratio)
+ panel.bwplot(..., col='black',
+              cex=2, pch='|', fill=c(blue, red,green), box.ratio =0.1)
+           panel.abline(h=0, col='grey')
+ },
+ par.settings = list(box.rectangle=list(col='black',lwd=2),
+ plot.symbol = list(pch='.', cex=4,lwd= 1)))
+dev.off()
+ 
+pdf("pruebasboxFixed.pdf")
+bwplot(value~variable, groups=variable,data=dferr2, ylab='Error kWh/kWp', xlab='model',
+       panel = function(...,box.ratio) {
+       panel.bwplot(..., col='black',
+ cex=2, pch='|', fill='gray', box.ratio =1)
+ },
+ par.settings = list(box.rectangle=list(col='black',lwd=2),
+ plot.symbol = list(pch='.', cex=4,lwd= 1)))
+dev.off()
 
+## con colores
+
+red <- "#E41A1C"
+blue <- "#377EB8"
+green <- "#4DAF4A"
+
+
+pdf("aFixed.pdf")
+bwplot(value~variable, groups=variable,data=dferrPhoto, ylab='Error kWh/kWp', xlab='model',
+       panel = function(...,box.ratio) {
+       panel.bwplot(..., col='black',
+ cex=2, pch='|', fill=c(blue,red, green), box.ratio =1)
+ },
+ par.settings = list(box.rectangle=list(col='black',lwd=2),
+ plot.symbol = list(pch='.', cex=4,lwd= 1)))
+dev.off()
 
 ## summary(err3)
 ##      Index           CAER              CNO              SAT         
@@ -400,9 +361,4 @@ xyplot(err3, scales = list(x = list(at = index(err3), rot=45)), type='p', ylab='
 )
 dev.off()
 
-###############################
-## agregar por meses
-
-month <- function(x) format(as.Date(x, '%m'))
-
-monPhoto <- format(as.Date(index(photocampa)), '%m')
+################
